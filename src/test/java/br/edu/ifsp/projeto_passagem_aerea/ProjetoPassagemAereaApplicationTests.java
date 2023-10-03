0761dc28e9538b42493ec49618b85ad662b2f057
Caio Arruda <bfjfhfgg@gmail.com>
2023-10-03 17:37:35 -0300

    Commit inicial criando estrutura projeto

---
+++ src/test/java/br/edu/ifsp/projeto_passagem_aerea/ProjetoPassagemAereaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.projeto_passagem_aerea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPassagemAereaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
