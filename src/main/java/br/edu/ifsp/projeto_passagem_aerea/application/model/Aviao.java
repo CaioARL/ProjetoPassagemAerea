88d247674c8986dbb5b7568d9b6ac31cf703c129
Caio Arruda <bfjfhfgg@gmail.com>
2023-10-03 23:19:10 -0300

    reset project

---
+++ src/main/java/br/edu/ifsp/projeto_passagem_aerea/application/model/Aviao.java
@@ -0,0 +1,15 @@
+package br.edu.ifsp.projeto_passagem_aerea.application.model;
+
+public class Aviao {
+
+	private String modelo;
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	
+}
