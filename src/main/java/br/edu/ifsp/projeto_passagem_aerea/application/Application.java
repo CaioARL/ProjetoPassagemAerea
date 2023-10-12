fb9a744c202b02ab6c7d9d95c8caea706fa0cd00
Caio Arruda <bfjfhfgg@gmail.com>
2023-10-12 20:38:29 -0300

    Criando metodo para cadastro de aviao e aeronave

--- src/main/java/br/edu/ifsp/projeto_passagem_aerea/application/Application.java
+++ src/main/java/br/edu/ifsp/projeto_passagem_aerea/application/Application.java
@@ -1,13 +1,18 @@
 package br.edu.ifsp.projeto_passagem_aerea.application;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import br.edu.ifsp.projeto_passagem_aerea.application.service.VooService;
+
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		
+		VooService vooService = new VooService();
+		vooService.cadastro();
+		
 	}
 
 }
